d9ad1f2845ff7aca5491e44ef511f8fc010ad352
zyh <2941412991@qq.com>
2026-01-08 00:05:43 +0800

    Add files via upload

---
+++ day_08/day_08/test.c
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("hehe\n");
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("haha\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i + 1;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+//Çó1£¡+2£¡+3£¡+4£¡+....+n!
+//int main()
+//{
+//	int n = 0;
+//	int i = 0;
+//	int ret = 1;
+//	int sum = 0;
+//	for (n = 1; n <= 3; n++)
+//	{
+//		ret = 1;
+//		for (i = 1; i <= n; i++)
+//		{
+//			ret *= i;
+//		}
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//} 
+int main()
+{
+	int n = 0;
+	int i = 0;
+	int ret = 1;
+	int sum = 0;
+	for (n = 1; n <= 3; n++)
+	{
+		ret *= n;
+		sum += ret;
+	}
+	printf("%d\n", sum);
+	return 0;
+} 
